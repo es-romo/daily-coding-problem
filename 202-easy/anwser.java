63144daf3bb238953442000edc5b9d26d9424f32
Esteban Romo <esteban.romo.98@gmail.com>
2020-10-25 19:27:25 -0400

    Day 202 ✅
    Just realized I've been calling them weeks..

---
+++ 202-easy/anwser.java
@@ -0,0 +1,21 @@
+/**
+ * anwser
+ */
+public class anwser {
+
+    public static void main(String[] args) {
+        System.out.println(palindrome(121));
+    }
+
+    public static boolean palindrome(int in) {
+
+        int x = in;
+        int y = 0;
+
+        while (x > 0) {
+            y = (y * 10) + (x % 10);
+            x /= 10;
+        }
+        return (in == y) ? true : false;
+    }
+}
