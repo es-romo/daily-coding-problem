9d6ef1e1345f7749bd2cf26f38e8a08d604717d4
Esteban Romo <esteban.romo.98@gmail.com>
2020-10-25 20:40:51 -0400

    Day 203 ✅

---
+++ 203-medium/anwser.py
@@ -0,0 +1,21 @@
+import math
+
+def bSearch(arr, mi, ma):
+    guess =(mi+ma)/2
+    if (arr[guess] <= arr[mi] & arr[guess] <= arr[ma]):
+        return arr[guess]
+    if ma-mi <= 1:
+        if arr[mi] < arr[ma]:
+            return arr[mi];
+        else:
+            return arr[ma];
+    else:
+        if arr[mi] < arr[ma]:
+            return bSearch(arr,mi,guess)
+        else:
+            return bSearch(arr,guess,ma)
+
+arr = [5, 7, 10, 3, 4]
+print( bSearch(arr,0,len(arr) - 1) )
+
+
