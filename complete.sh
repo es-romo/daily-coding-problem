19f0d04cae5bbaf6349ddf138081efedc65cb5b9
Esteban <esteban.romo.98@gmail.com>
2023-09-26 23:45:54 -0400

    Add "complete" script

---
+++ complete.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+if [ ! -z "$1" ]; then
+    dayNumber=$1
+else
+    read -p $'\e[36mEnter the day number: \e[0m' dayNumber
+fi
+
+if [ ! -d "problems/$dayNumber" ]; then
+    echo -e "\033[0;31mProblem $dayNumber does not exist"
+    exit 1
+fi
+
+source problems/$dayNumber/.info
+echo -e "difficulty=${difficulty}\nstatus=Complete" > problems/$dayNumber/.info
+
+echo -e $'\e[0;32mDone!'
