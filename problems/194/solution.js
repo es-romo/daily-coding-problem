1b5c844b13542d674fe485ef585438aa88571910
Esteban <esteban.romo.98@gmail.com>
2023-09-27 23:29:15 -0400

    Add solution 194

---
+++ problems/194/solution.js
@@ -0,0 +1,14 @@
+const intersectedSegments = (a, b) => {
+  let total = 0;
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (b[j] < a[i] && a[j] > b[i]) {
+        total++;
+      }
+    }
+  }
+  return total;
+};
+
+console.log(intersectedSegments([2, 3, 4, 5], [4, 5, 1, 3])); // 4
+console.log(intersectedSegments([1, 3, 4, 5], [3, 2, 5, 4])); // 2
